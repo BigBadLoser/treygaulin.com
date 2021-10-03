64fe778627625a8e1ecd5da2f99494396b6b9929
Trey Gaulin <loserizer@gmail.com>
2021-10-03 11:25:21 -0400

    Updates

--- component---src-pages-page-2-js-843776032b528fc85579.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktreygaulin_com=self.webpackChunktreygaulin_com||[]).push([[617],{9863:function(e,t,n){n.r(t);var l=n(7294),a=n(5444),c=n(4320),o=n(3751);t.default=function(){return l.createElement(c.Z,null,l.createElement(o.Z,{title:"Page two"}),l.createElement("h1",null,"Hi from the second page"),l.createElement("p",null,"Welcome to page 2"),l.createElement(a.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-js-843776032b528fc85579.js.map
